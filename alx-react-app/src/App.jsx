cdb47478e7d292c681dbb7bfa0645eca79aa3670
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-07-19 06:57:21 +0300

    More fixes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <>
-      <WelcomeMessage/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage/>
     </>
   )
 }
