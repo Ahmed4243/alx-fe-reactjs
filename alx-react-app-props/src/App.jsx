23b1a13ad4fe2e198c6d5189605b64ffea388e66
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-07-20 21:51:07 +0300

    Fix

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import UserContext from '../UserContext'; // ✅ import the context
+import UserContext from './UserContext'; // ✅ import the context
 
 function App() {
   const [count, setCount] = useState(0);
