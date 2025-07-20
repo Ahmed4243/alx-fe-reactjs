7cf7f536a08222529ab81eb30b0e7044d739fc70
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-07-20 21:36:07 +0300

    Included Counter

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 export default function MainContent() {
     return (
-    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '200px' }}>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '200px' }}>
+                <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
 }
