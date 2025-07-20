b73b5aa9b8fb1d51d6512c8c8a371628694f393e
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-07-20 21:33:16 +0300

    Fixes

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 export default function MainContent() {
     return (
-        <main>
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '200px' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
